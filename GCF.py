bc537a23e28e84ffd02339a38e072b71cabca126
BlineraHysenaj <52405935+BlineraHysenaj@users.noreply.github.com>
2020-03-27 01:10:08 +0100

    Disa nga metodat

---
+++ GCF.py
@@ -0,0 +1,11 @@
+import math
+x = input("Numri i pare eshte: ")
+y = input("Numri i dyte eshte: ")
+
+x = int(x)  
+y = int(y)
+
+print ("Faktori me i madh i perbashket eshte: ",math.gcd(x, y))
+
+#Referenca:
+#https://www.tutorialspoint.com/gcd-function-python
---
+++ TIME.py
@@ -0,0 +1,4 @@
+import datetime
+now = datetime.datetime.now()
+print ("Koha aktuale eshte : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
